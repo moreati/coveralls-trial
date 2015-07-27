7fa7024ea4e528725ead11b54601bb66f9869363
Alex Willmer <alex@moreati.org.uk>
2015-07-27 16:23:54 +0200

    Add foo()

--- coveralls_trial/coveralls_trial.py
+++ coveralls_trial/coveralls_trial.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
 
-import re
-import sys
-
-if sys.version_info[:2] >= (3, 0):
-    text_type = str
-else:
-    text_type = unicode
+def foo():
+    return True
--- tests/test_coveralls_trial.py
+++ tests/test_coveralls_trial.py
@@ -2,6 +2,3 @@
 
 import unittest
 from coveralls_trial.coveralls_trial import *
-
-if __name__ == '__main__':
-    unittest.main()
