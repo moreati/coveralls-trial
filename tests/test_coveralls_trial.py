dc75679ed3aaf53618bedf0fe0bb4927fa045e3a
Alex Willmer <alex@moreati.org.uk>
2015-07-27 18:13:10 +0200

    Whom

--- tests/test_coveralls_trial.py
+++ tests/test_coveralls_trial.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-from coveralls_trial.coveralls_trial import *
+from coveralls_trial import coveralls_trial
 
 class Tests(unittest.TestCase):
     def test_foo(self):
-        self.assertEqual(True, foo())
+        self.assertEqual(True, coveralls_trial.foo())
 
     def test_bar(self):
-        self.assertEqual(False, bar())
+        self.assertEqual(False, coveralls_trial.bar())
 
 #    def test_baz(self):
-#        self.assertEqual(4, baz())
+#        self.assertEqual(4, coveralls_trial.baz())
